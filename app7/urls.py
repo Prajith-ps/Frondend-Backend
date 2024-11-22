d07ae0f9e8b5e1abf8738c4b030dfb0c970ce6d3
prajith <prajithps212003@gmail.com>
2024-11-22 16:07:14 +0530

    first commit

--- app7/urls.py
+++ app7/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('about/',views.about,name='about'),
+     path('contact/',views.contact,name='contact'),
+     path('login/',views.login,name='login'),
+      path('register/',views.register,name='register'),
 ]
 
--- app7/views.py
+++ app7/views.py
@@ -3,3 +3,16 @@ from django.template import loader
 def home(request):
     template=loader.get_template('index.html')
     return HttpResponse(template.render())
+def about(request):
+    template=loader.get_template('about.html')
+    return HttpResponse(template.render())
+
+def contact(request):
+    template=loader.get_template('contact.html')
+    return HttpResponse(template.render())
+def login(request):
+    template=loader.get_template('login.html')
+    return HttpResponse(template.render())
+def register(request):
+    template=loader.get_template('register.html')
+    return HttpResponse(template.render())
